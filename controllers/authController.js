20a48c119f995098a91cc6d64d9d3fd1f83048d2
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2025-07-28 12:12:44 +0500

    Added Refresh Token

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,6 @@ const registerUser = async (req, res) => {
     // Logic for registering a user
     try {
         const { userName, email, password, phone, address } = req.body;
-        console.log(req.body)
         if (!userName || !email || !password || !phone || !address) {
             return res.status(400).json({ success: false, message: 'All fields are required' });
         }
@@ -45,10 +44,28 @@ const loginUser = async (req, res) => {
             if (!isMatch) {
                 return res.status(401).json({ success: false, message: 'Invalid credentials' });
             }
-            res.status(200).json({ success: true, message: 'User logged in successfully', user });
+            const accessToken = jwt.sign(
+                { userId: user._id },
+                process.env.JWT_SECRET,
+                { expiresIn: '1h' }
+            );
+
+            const refreshToken = jwt.sign(
+                { userId: user._id },
+                process.env.JWT_REFRESH_SECRET, // Use a separate secret
+                { expiresIn: '7d' }
+                
+            );
+
+            // You can store the refreshToken in DB if needed or send it as an HttpOnly cookie
+            res.json({
+                success: true,
+                message: 'User logged in successfully',
+                user,
+                accessToken,
+                refreshToken,
+            });
         }
-
-
     } catch (error) {
         console.log(error)
         res.status(500).json({ success: false, message: 'Error in login user', error: error.message });
