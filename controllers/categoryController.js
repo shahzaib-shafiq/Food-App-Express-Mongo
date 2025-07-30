952a0e74bad7f5a9993776286d53fe89ca70477f
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2025-07-30 12:20:05 +0500

    Added Category and Food Api

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,6 @@
 import categoryModel from '../models/categoryModel.js';
 const createCateogry = async (req, res) => {
     try {
-        console.log("Creating category with data:", req.body);
         const { title, imageUrl } = req.body;
         if (!title) {
             return res.status(400).json({
@@ -26,4 +25,103 @@ const createCateogry = async (req, res) => {
     }
 };
 
-export { createCateogry };
+const getAllCateogry = async (req, res) => {
+    try {
+        const categories = await categoryModel.find({});
+        res.status(200).json({
+            success: true,
+            message: "All Categories Fetched Successfully",
+            categories,
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error Fetching category",
+            error: error.message,
+        });
+    }
+};
+const updateCateogry = async (req, res) => {
+    try {
+        const { title, imageUrl } = req.body;
+         if (!req.params.id) {
+            return res.status(400).send({
+                success: false,
+                message: "Category ID is required",
+            });
+        }
+        const categories = await categoryModel.findById({_id: req.params.id});
+        if (!categories) {
+            return res.status(404).send({
+                success: false,
+                message: "Category not found",
+            });
+        }
+
+        categories.title=title;
+        categories.imageUrl=imageUrl;
+        await categories.save();
+        res.status(200).json({
+            success: true,
+            message: "Category Updated Successfully",
+            categories,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error Fetching category",
+            error: error.message,
+        });
+    }
+};
+const deleteCateogry = async (req, res) => {
+    try {
+          if (!req.params.id) {
+            return res.status(400).send({
+                success: false,
+                message: "Category ID is required",
+            });
+        }
+        const categories = await categoryModel.findOneAndDelete({_id: req.params.id});
+        res.status(200).json({
+            success: true,
+            message: "Category Deleted Successfully",
+            categories,
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error Deleting Category",
+            error: error.message,
+        });
+    }
+};
+
+const getCateogryById = async (req, res) => {
+    try {
+         if (!req.params.id) {
+            return res.status(400).send({
+                success: false,
+                message: "Category ID is required",
+            });
+        }
+        const category = await categoryModel.findOne({_id: req.params.id});
+        res.status(200).json({
+            success: true,
+            message: "Category Fetched Successfully",
+            category,
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error Fetching category",
+            error: error.message,
+        });
+    }
+};
+
+export { createCateogry,getAllCateogry,updateCateogry,deleteCateogry,getCateogryById };
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import {createCateogry} from '../controllers/categoryController.js';
+import {createCateogry,getAllCateogry,deleteCateogry,updateCateogry,getCateogryById} from '../controllers/categoryController.js';
 const router = express.Router()
 
 router.post('/create',authMiddleware,createCateogry)
+router.get('/all',authMiddleware,getAllCateogry)
+router.get('/:id',authMiddleware,getCateogryById)
+router.patch('/update/:id',authMiddleware,updateCateogry)
+router.delete('/delete/:id',authMiddleware,deleteCateogry)
 
 export default router;
